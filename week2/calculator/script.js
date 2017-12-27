30cb236250db1f7b5f92a9683808b2172271c71c
Devon Johnson <dseth.johnson@GMAIL.COM>
2017-12-27 04:24:30 -0500

    week2 review projects

---
+++ week2/calculator/script.js
@@ -0,0 +1,14 @@
+var calculator = {
+    add: function(a, b){
+       value = (return a + b)
+    },
+    subtract: function(a, b){
+        value = (return a - b)
+     },
+     multiply: function(a, b){
+        value = (return a * b)
+     },
+     divide: function(a, b){
+        value = (return a/b)
+     }
+}
