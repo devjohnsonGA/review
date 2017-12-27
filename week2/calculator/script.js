947d0740f2f1ea9a493313b9614a912f319d267d
Devon Johnson <dseth.johnson@GMAIL.COM>
2017-12-27 04:37:42 -0500

    calc working!

--- week2/calculator/script.js
+++ week2/calculator/script.js
@@ -1,14 +1,19 @@
 var calculator = {
     add: function(a, b){
-       value = (return a + b)
+       return a + b
     },
     subtract: function(a, b){
-        value = (return a - b)
+        return a - b
      },
      multiply: function(a, b){
-        value = (return a * b)
+        return a * b
      },
      divide: function(a, b){
-        value = (return a/b)
+        return a/b
      }
 }
+
+console.log(calculator.add(1, 3))
+console.log(calculator.subtract(9, 4))
+console.log(calculator.multiply(2, 3))
+console.log(calculator.divide(21, 3))
